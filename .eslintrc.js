4651a1b7d2f10d54713de420329b4c1a63944671
Dallas <71103219+LeleDallas@users.noreply.github.com>
2024-07-01 19:18:12 +0200

    chore(prettier): update settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': 0,
+    'no-console': 1,
+    'no-undef': 1,
+    'no-unused-vars': 1,
+    'react-native/no-inline-styles': 0,
+  },
 };
