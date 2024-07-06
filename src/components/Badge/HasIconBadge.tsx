428c67465b7a6e04f9aab164afb4a7a0eac02496
Dallas <71103219+LeleDallas@users.noreply.github.com>
2024-07-06 14:21:47 +0200

    feat(Badge): Icon badge

---
+++ src/components/Badge/HasIconBadge.tsx
@@ -0,0 +1,27 @@
+import { View } from 'react-native';
+import Octicons from 'react-native-vector-icons/Octicons';
+
+type Props = {
+    hasBadge: boolean;
+    name: string;
+}
+export default (props: Props) => {
+    const { hasBadge, name } = props;
+
+    return (
+        hasBadge &&
+        <View style={{
+         flexDirection: 'row',
+            alignItems: 'center',
+            alignSelf: 'flex-start',
+            borderRadius: 4,
+            borderWidth: 1,
+            borderColor: '#e0e0e0',
+            paddingHorizontal: 8,
+            paddingVertical: 4,
+            gap: 8
+        }}>
+            <Octicons name={name} color="#fff" size={14} />
+        </View>
+    );
+};
