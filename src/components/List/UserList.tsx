acc15d983f18b9eec9c9ca354d50a46575b51320
Dallas <71103219+LeleDallas@users.noreply.github.com>
2024-07-08 23:04:18 +0200

    feat(List): uset and repo list

--- src/components/List/UserList.tsx
+++ src/components/List/UserList.tsx
@@ -1,21 +1,24 @@
 import { VirtualizedList } from 'react-native';
 import { User } from '../../types/response';
 import UserCard from '../Card/UserCard';
+import Spinner from '../Spinner';
 
 type Props = {
     users: User[];
+    loading: boolean;
 }
 
 export default (props: Props) => {
-    const { users } = props;
+    const { users, loading } = props;
     return (
-        <VirtualizedList<User>
-            style={{ flex: 1, marginTop: 8 }}
-            data={users}
-            renderItem={({ item }) => <UserCard user={item} />}
-            keyExtractor={(item) => JSON.stringify(item.id) ?? Math.random().toString(36).substring(7)}
-            getItemCount={(data) => data.length}
-            getItem={(data, index) => data[index]}
-        />
+        loading ? <Spinner /> :
+            <VirtualizedList<User>
+                style={{ flex: 1, marginTop: 8 }}
+                data={users}
+                renderItem={({ item }) => <UserCard user={item} />}
+                keyExtractor={(item) => JSON.stringify(item.id) ?? Math.random().toString(36).substring(7)}
+                getItemCount={(data) => data.length}
+                getItem={(data, index) => data[index]}
+            />
     );
 };
