56d557b5533dc35498ad49f5b9d62e7aab64cbef
Dallas <71103219+LeleDallas@users.noreply.github.com>
2024-07-09 20:40:42 +0200

    style(android): notch color

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { NavigationContainer } from '@react-navigation/native';
 import React, { useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
+import { Platform, SafeAreaView, StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MainRoute from './src/routes/MainRoute';
-import { globalStyle, theme } from './src/styles';
+import { colors, globalStyle, theme } from './src/styles';
 import SplashScreen from 'react-native-splash-screen';
 
 export default () => {
-  
+
   useEffect(() => SplashScreen?.hide(), []);
 
-  return <SafeAreaView style={globalStyle.background} >
+  return <SafeAreaView style={[globalStyle.background, {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }]} >
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer theme={theme}>
         <BottomSheetModalProvider>
+          <StatusBar backgroundColor={colors.background} barStyle="light-content" />
           <MainRoute />
         </BottomSheetModalProvider>
       </NavigationContainer>
